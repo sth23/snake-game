0a5e5286d75f3b834ab66cf30632f922efdb52b4
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-06 14:55:26 -0400

    Updated from Brython Server: 5/6/2019 2:55:28 PM

--- snake-game.py
+++ snake-game.py
@@ -1,1 +1,28 @@
+"""
+Final Project: Snake Game
+Author: Sean
+Credit: Tutorials
 
+Assignment:
+Create an old-school snake game
+"""
+
+from ggame import App, RectangleAsset, CircleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+import math
+import random
+
+class Wall(Sprite):
+    # Colors
+    black = Color(0,1)
+    noline = LineStyle(0,black)
+    # Rectangle Assets
+    topwalls = RectangleAsset(self.width, 10, noline, black)
+    sidewalls = RectangleAsset(10, self.height, noline, black)
+    
+
+        
+
+class SnakeGame(app):
+    
+    def __init__(self):
+        super().__init__()
